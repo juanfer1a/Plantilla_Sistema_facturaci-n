23f067e87c0b61d4fd26fcfa88506841bcc72fe0
juan quiceno <juan.quiceno037@pascualbravo.edu.co>
2022-02-19 23:04:25 -0500

    validar usuario

--- “Plantilla_Sistema_facturación”/frmlogin.Designer.cs
+++ “Plantilla_Sistema_facturación”/frmlogin.Designer.cs
@@ -31,12 +31,12 @@ namespace _Plantilla_Sistema_facturación_
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmlogin));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.lblTitulo = new MaterialSkin.Controls.MaterialLabel();
             this.txtUsuario = new MaterialSkin.Controls.MaterialSingleLineTextField();
             this.txtPassword = new MaterialSkin.Controls.MaterialSingleLineTextField();
             this.btnValidar = new MaterialSkin.Controls.MaterialRaisedButton();
             this.btnCancelar = new MaterialSkin.Controls.MaterialRaisedButton();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -51,16 +51,26 @@ namespace _Plantilla_Sistema_facturación_
             this.panel1.Size = new System.Drawing.Size(202, 250);
             this.panel1.TabIndex = 0;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(22, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(154, 145);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // lblTitulo
             // 
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.Depth = 0;
-            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Font = new System.Drawing.Font("Roboto", 11F);
             this.lblTitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.lblTitulo.Location = new System.Drawing.Point(309, 38);
             this.lblTitulo.MouseState = MaterialSkin.MouseState.HOVER;
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(154, 18);
+            this.lblTitulo.Size = new System.Drawing.Size(136, 19);
             this.lblTitulo.TabIndex = 1;
             this.lblTitulo.Text = "VALIDAR USUARIO";
             // 
@@ -112,6 +122,7 @@ namespace _Plantilla_Sistema_facturación_
             this.btnValidar.TabIndex = 4;
             this.btnValidar.Text = "VALIDAR";
             this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
             // 
             // btnCancelar
             // 
@@ -128,16 +139,6 @@ namespace _Plantilla_Sistema_facturación_
             this.btnCancelar.Text = "CANCELAR";
             this.btnCancelar.UseVisualStyleBackColor = true;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(22, 50);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(154, 145);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // frmlogin
             // 
--- “Plantilla_Sistema_facturación”/frmlogin.cs
+++ “Plantilla_Sistema_facturación”/frmlogin.cs
@@ -21,5 +21,29 @@ namespace _Plantilla_Sistema_facturación_
         {
             Application.Exit();
         }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            string usuario = "";//Varible para validar con la BD el usuario
+            if (txtUsuario.Text=="admin" && txtPassword.Text=="123")//Valida usuario y contraseña 
+            {
+                usuario = "Juan Quiceno";
+                frmPrincipal frmPpal = new frmPrincipal();//objeto tipo formPrincipal 
+                this.Hide();//Hacemos invisible el login
+                frmPpal.Show();//Mostramos el principal
+            }
+            if (usuario != string.Empty)
+            {
+                MessageBox.Show("Usuario valido Bienvenido" + usuario);
+            }
+            else
+            {
+                MessageBox.Show("USUARIO O CONTRASEÑA INVALIDA INTENTELO DE NUEVO");
+                txtUsuario.Text = string.Empty;
+                txtPassword.Text = string.Empty;
+                txtUsuario.Focus();
+            }
+
+        }
     }
 }
